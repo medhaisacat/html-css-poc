2132d3a9acc6297156f8d25add70d011453f8315
Medha S N <meedddhhhhaaaaa@gmail.com>
2022-06-15 13:12:02 +0530

    Add javascript

---
+++ app.js
@@ -0,0 +1,15 @@
+// var images = document.getElementsByTagName('img');
+
+// for(let image of images) {
+//     console.log("Image properties: ", image);
+// }
+
+// var headings = document.querySelectorAll('h1');
+
+// for(let heading of headings) {
+//     heading.innerText = "You are hacked!"
+// }
+
+// // var logo = document.querySelector('h1');
+
+// // logo.className = "blueLogo";
